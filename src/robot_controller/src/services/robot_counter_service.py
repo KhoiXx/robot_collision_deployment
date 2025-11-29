4dc84c52cca63a3883d96569c4b21f290f18892f
KhoiXx <tankhoi.kn@gmail.com>
2025-11-29 20:53:48 +0700

    fix: update launch missing robot urdf

--- src/robot_controller/src/services/robot_counter_service.py
+++ src/robot_controller/src/services/robot_counter_service.py
@@ -17,6 +17,10 @@ class RobotCounterService:
             rospy.wait_for_service('/get_robot_counter', timeout=1)
         except rospy.ROSException:
             rospy.Service('get_robot_counter', Trigger, self.handle_get_counter)
+        try:
+            rospy.wait_for_service('/reset_robot_counter', timeout=1)
+        except rospy.ROSException:
+            rospy.Service('reset_robot_counter', Trigger, self.handle_reset)
         rospy.loginfo("robot_counter_service is ready")
     
     def handle_request(self, request):
@@ -26,7 +30,13 @@ class RobotCounterService:
     
     def handle_get_counter(self, request):
         return TriggerResponse(success=True, message=str(self.robot_count))
-    
+
+    def handle_reset(self, request):
+        """Reset robot counter to 0"""
+        self.robot_count = 0
+        rospy.loginfo("Robot counter reset to 0")
+        return TriggerResponse(success=True, message="Counter reset to 0")
+
 if __name__ == "__main__":
     rospy.init_node('robot_counter_service')
     RobotCounterService()
